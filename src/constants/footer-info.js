f3b3e45033307de5673b01ed937146782b82a9b3
V2 Dev <v2.softwarehouse@gmail.com>
2024-08-29 09:54:27 -0300

    start zcex project

---
+++ src/constants/footer-info.js
@@ -0,0 +1,7 @@
+export const FOOTER_TITLES = [
+    {text: "Documentation", url: "#", id: 1}, 
+    {text: "GitHub", url: "https://github.com/suportezcex/zcex"},
+    {text: "LinkedIn", url: ""}
+]
+
+export const FOOTER_CONTACT_BTN = {text: "Contact us"}
