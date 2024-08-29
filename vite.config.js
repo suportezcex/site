eafdf2d83d43ef7232cd58faab7916fd90fa005e
Vinnie <vabsbc@gmail.com>
2024-08-29 15:31:46 -0300

    up deploy config

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default defineConfig({
+  base: "./",
   plugins: [react()],
   resolve: {
     alias: {
