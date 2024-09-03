013915baf3d7b6e15c2c5b4ca41ccb58ec7e5b80
Vinnie <vabsbc@gmail.com>
2024-09-03 18:58:54 -0300

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default defineConfig({
-  base: "./",
+  base: "/site/",
   plugins: [react()],
   resolve: {
     alias: {
